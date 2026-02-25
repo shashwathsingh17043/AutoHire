afd5d07bedad67c3b5cb79d727c406f87863627e
Dhruvi240206 <dhruvisuri9@gmail.com>
2026-02-25 16:35:51 +0530

    Improved scoring logic with missing skill detection

--- app/scorer.py
+++ app/scorer.py
@@ -1,4 +1,16 @@
 def calculate_score(found_skills, required_skills):
-    match_count = len(set(found_skills) & set(required_skills))
+
+    if not required_skills:
+        return 0
+
+    match_count = 0
+    missing_skills = []
+
+    for skill in required_skills:
+        if skill in found_skills:
+            match_count += 1
+        else:
+            missing_skills.append(skill)
+
     score = (match_count / len(required_skills)) * 100
-    return score
+    return round(score, 2)
